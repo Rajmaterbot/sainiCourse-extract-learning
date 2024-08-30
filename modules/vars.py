12f2affc4c0932611d85c4568fc608b5f6307bf3
Raj Saini <98858458+Rajmaterbot@users.noreply.github.com>
2024-08-30 23:33:26 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "24120186"
-api_hash = "01fd2c9e36d0754b9fd24f15359fe22e"
+api_id = "16712603"
+api_hash = "62e7bdb76c9d12f90021a5a90bb2402f"
 bot_token = "7478658268:AAGRPWhmfHjUH8fZ0TD49-3J4DrtJHKKY-c"
